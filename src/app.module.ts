3f5e633a777c0c712cb13df48d411a4ef4fa7e3e
Jose Matias <josematias.dev@gmail.com>
2021-06-20 18:00:41 -0300

    feat: add mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const linkMongo = 'mongodb://dbtest:dbtest@localhost:27017/api-nestjs-mongo';
+
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(linkMongo)],
   controllers: [AppController],
   providers: [AppService],
 })
