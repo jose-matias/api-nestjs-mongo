709fcb88efdc6ca66d1823fd3e9a71bf7285538f
Jose Matias <josematias.dev@gmail.com>
2021-06-20 22:07:07 -0300

    feat: add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 const linkMongo = 'mongodb://dbtest:dbtest@localhost:27017/api-nestjs-mongo';
 
 @Module({
-  imports: [MongooseModule.forRoot(linkMongo)],
+  imports: [MongooseModule.forRoot(linkMongo, { useFindAndModify: false }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
